698c593f0c5dd0cc90d2a17afcd0c68277417083
imchic <lhyobin44@gmail.com>
2023-01-12 11:34:51 +0900

    Timber modify

--- app/src/main/java/com/example/hiltsample/App.kt
+++ app/src/main/java/com/example/hiltsample/App.kt
@@ -9,8 +9,24 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        BuildConfig.DEBUG.let {
-            Timber.plant(Timber.DebugTree())
+//        BuildConfig.DEBUG.let {
+//            Timber.plant(Timber.DebugTree())
+//        }
+        initLogger()
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String? {
+                    return String.format(
+                        "%s.%s(L:%s)",
+                        super.createStackElementTag(element),
+                        element.methodName,
+                        element.lineNumber
+                    )
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/example/hiltsample/NetworkModule.kt
+++ app/src/main/java/com/example/hiltsample/NetworkModule.kt
@@ -1,5 +1,8 @@
 package com.example.hiltsample
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import com.google.gson.JsonSyntaxException
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,6 +11,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -20,13 +24,30 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideBaseUrl () = BASE_URL
+    fun provideBaseUrl() = BASE_URL
 
     @Provides
     @Singleton
     fun httpLoggingInterceptor(): HttpLoggingInterceptor =
-        HttpLoggingInterceptor()
-            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        //HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                if (!message.startsWith("{") && !message.startsWith("[")) {
+                    Timber.tag("OkHttp").d(message)
+                    return
+                }
+                try {
+                    // Timber 와 Gson setPrettyPrinting 를 이용해 json 을 보기 편하게 표시해준다.
+                    Timber.tag("OkHttp")
+                        .d(GsonBuilder().setPrettyPrinting().create().toJson(JsonParser().parse(message)))
+                } catch (m: JsonSyntaxException) {
+                    Timber.tag("OkHttp").d(message)
+                }
+            }
+
+        }).apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
 
     @Provides
     @Singleton
